ece364bc80d6b127a7982f584f472c96538d4bea
Programito <Programito@gmail.com>
2019-11-03 18:12:45 +0100

    rutas sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StylesService } from '../../services/service.index';
+import { Usuario } from '../../models/usuario.model';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -8,9 +10,20 @@ import { StylesService } from '../../services/service.index';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor(
-    public sytleService: StylesService
-  ) { }
+  id: String;
+  admin: Boolean = false;
+  nombre: String;
+
+  constructor(public sytleService: StylesService) {
+    let user = JSON.parse(localStorage.getItem('usuario'));
+
+    this.id = user[0]._id;
+    this.nombre = user[0].nombre;
+    if (user[0].role === 'ADMIN_ROLE') {
+      this.admin = true;
+    }
+
+   }
 
   ngOnInit() {
   }
