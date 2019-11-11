d603cb0171416a5703da69406842ca0b68c236cf
Programito <Programito@gmail.com>
2019-11-11 09:54:45 +0100

    page

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,4 +2,4 @@
 
 // export const URL_ECOMMERCE = 'http://localhost:3000';
 
-export const URL_ECOMMERCE = 'http://oneforall-ecommerce.herokuapp.com';
+export const URL_ECOMMERCE = 'https://oneforall-ecommerce.herokuapp.com';
